921b6d87c5557b50490ebf89c61dc7a6e1f530c0
whatalunatik <whatalunatik@gmail.com>
2015-02-10 03:04:37 -0500

    It works!
    My first p5.sound project.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -6,7 +6,7 @@ function setup()
   createCanvas(800,800);
   allnotes = new Array();
   println(allnotes);
-  speed = random(10)/10;
+  speed = (random(10)+1)/10;
 }
 
 function draw() 
@@ -31,13 +31,34 @@ function Note(x, y, speed)
 {
 	this.x = x;
 	this.y = y;
+
 	
 	this.speed = speed;
-	var voice = new p5.Oscillator((random(500)+200), "tri");
-	voice.amp(0.1,0);
+	
+	var freq = (random(500)+200);
+
+	println(freq*(2/(speed)));
+
+	var voice = new p5.Oscillator(freq, "sawtooth");
+	var env = new p5.Env(0.005, (speed/2), (speed * 4));
+	var filter = new p5.BandPass();
+	
+	voice.disconnect();
+	voice.connect(filter);
+
+	filter.freq(freq*(1/(speed)));
+	filter.res(freq/14);
+	
+	voice.amp(0 , 0);
+	voice.amp(env);
+	
+
 	voice.start();
-	println(voice);
-	println(speed);
+	
+
+	//println(voice);
+	//println(speed);
+	//println(filter);
 
 
 	this.update = function()
@@ -68,8 +89,7 @@ function Note(x, y, speed)
 
 	var playSound = function()
 	{
-		voice.amp((speed * 0.5), .033);
-		voice.amp(0, (speed * 2));
+		env.play();
 		//Println("Success?");
 
 	};
